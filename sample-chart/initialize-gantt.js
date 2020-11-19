2f3728126fde98332cb2d209dd332f5b1e6e19c4
MacDonald <mmacdonald@UFL.EDU>
2020-11-19 14:35:19 -0500

    Change sample to use groupBy

--- sample-chart/initialize-gantt.js
+++ sample-chart/initialize-gantt.js
@@ -4,21 +4,40 @@ let data =  [
             row: "Row for ID #1",
             tooltip: "Tooltips here! Get your tooltips!",
             start: "Wed Jun 03 2020 14:21:55",
-            end: "Wed Jun 03 2020 20:21:55"
+            end: "Wed Jun 03 2020 20:21:55",
+            group: "This is a group with a new name",
+            groupId: 5
         },
         {
             recordID: 2,
             row: "Row for ID #2",
             tooltip: "Tooltip for row 2",
             start: "Jun 03 2020 11:00:00",
-            end: "Jun 03 2020 15:23:43"
+            end: "Jun 03 2020 15:23:43",
+            group: "empty",
+            groupId: 1
         },
         {
             recordID: 1,
             row: "Row for ID #1",
             tooltip: "Tooltip unique to this item",
             start: "Wed Jun 03 2020 06:00:00",
-            end: "Wed Jun 03 2020 10:00:00"
+            end: "Wed Jun 03 2020 10:00:00",
+            group: "test",
+            groupId: 2,
+            subGroupId: 1,
+            subGroup: "Subgroup #1"
+        },
+        {
+            recordID: 5,
+            row: "Now we have grouping",
+            tooltip: "Tooltip after grouping",
+            start: "Wed Jun 03 2020 06:00:00",
+            end: "Wed Jun 03 2020 10:00:00",
+            group: "test",
+            groupId: 2,
+            subGroupId: 1,
+            subGroup: "Subgroup #1"
         }
     ];
 
@@ -38,7 +57,8 @@ let params = {
     rowAlias: "row",
     linkAlias: null,
     tooltipAlias: "tooltip",
-    groupBy: null,
+    groupBy: "groupId,subGroupId",
+    groupByAlias: "group,subGroup",
     refreshFunction: refreshFunction
 }
 
